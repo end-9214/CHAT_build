b65dc05e862b84551230abf9fdccdf58893f129e
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-08-03 09:28:13 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,18 @@ app.use(cors({ origin: true }));
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
 
+  try{
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      {username: username, secret: username, first_name: username, last_name: username},
+      {headers: { "Private-Key": "e7a12977-2321-4a05-a3e5-51296d0cb349" }}
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 
+  
   return res.json({ username: username, secret: "sha256..." });
 });
 
